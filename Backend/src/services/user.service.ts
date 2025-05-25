8c0f1a5ab79e2b947eca97352acf06b920936752
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-25 12:00:00 +0200

    feat : added service for create user function

--- Backend/src/services/user.service.ts
+++ Backend/src/services/user.service.ts
@@ -1,8 +1,14 @@
 import { getByNumericId  } from '../models/user.model';
+import { createUserQuery } from '../models/user.model';
 
 export async function getUserByNumericId (id : number){
 
     const userInfo =   await getByNumericId(id);
 
+    return userInfo;
+}
+
+export async function createUser(id: string, first_name: string, last_name: string, birthday: Date, location: string){
+    const userInfo = await createUserQuery(id, first_name, last_name, birthday, location);
     return userInfo;
 }

b7118dbde248362b6f95b40abe10a572fdd6534a
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-25 12:00:00 +0200

    feat : added controller for create user function

--- Backend/src/controllers/user.controller.ts
+++ Backend/src/controllers/user.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
-import { getUserByNumericId } from "../services/user.service";
+import { getUserByNumericId} from "../services/user.service";
+import { createUser }from "../services/user.service";
 
 export const getUserProfil = async (
     req: Request,
@@ -14,4 +15,29 @@ export const getUserProfil = async (
 
     const userInfo = await getUserByNumericId(id);
     res.json(userInfo);
+};
+
+
+export const userCreation = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { id, first_name, last_name, birthday, location } = req.body;
+
+        if (!id || !first_name || !last_name || !birthday) {
+            return res.status(400).json({ error: "Champs requis manquants" });
+        }
+
+        const birthdayDate = new Date(birthday);
+        if (isNaN(birthdayDate.getTime())) {
+            return res.status(400).json({ error: "Date d'anniversaire invalide" });
+        }
+
+        const newUser = await createUser(id, first_name, last_name, birthdayDate, location);
+        return res.status(201).json(newUser);
+    } catch (error) {
+        next(error);
+    }
 };

c4db0f249ca922ca5dd0a9d0a0441db64d1ac0d0
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-26 12:00:00 +0200

    fix: navigation logic

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './logic/login';
-import Dashboard from "./view/dashboard/dashboard.tsx";
+import Dashboard from './logic/dashboard/dashboard.tsx';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/" element={<Dashboard />} />
+            </Routes>
+        </BrowserRouter>
+    );
 };
 
 export default App;

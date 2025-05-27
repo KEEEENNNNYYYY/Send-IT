b05f53fa0f748f236b057f6f66dc4a18e5c5ed42
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-27 13:06:08 +0200

    feat : added a new page to / endpoint

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './logic/login'; 
+import Login from './logic/login';
+import Dashboard from "./view/dashboard/dashboard.tsx";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Login />} /> 
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );

f709b28b32ad4c7b45a0f9cb82a1bd645b03c32f
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-29 09:58:25 +0200

    fix : updated missing column

--- Backend/db/migration/0001_create_user_table.sql
+++ Backend/db/migration/0001_create_user_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE "user" (
     first_name VARCHAR NOT NULL,
     last_name VARCHAR NOT NULL,
     birthday DATE NOT NULL,
+    email VARCHAR NOT NULL,
     numeric_id SERIAL UNIQUE NOT NULL,
     location VARCHAR,
     creation_date DATE

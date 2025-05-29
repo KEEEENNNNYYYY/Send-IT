569584dadf23067602f6d3953a21a8343863087a
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-29 09:59:26 +0200

    chore : removed blank space

--- Front-end/src/logic/login/login.tsx
+++ Front-end/src/logic/login/login.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
-// @ts-ignore
 import { auth } from "../../../firebase";
 import LoginView from "../../view/login/login.tsx";
 

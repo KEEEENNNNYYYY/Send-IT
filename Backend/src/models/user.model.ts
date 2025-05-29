c167ca15b7e8cba9ae5ff4c93abde3347f256e9d
KEEEENNNNYYYY <hei.kenny.30904@gmail.com>
2025-05-29 09:08:26 +0200

    feat : create model function

--- Backend/src/models/user.model.ts
+++ Backend/src/models/user.model.ts
@@ -11,3 +11,22 @@ export async function getByNumericId(id: number) {
     winston.error("error", error);
   }
 }
+
+export async function createUserQuery(id: string, first_name: string, last_name: string, birthday: Date, location: string) {
+  const query =
+    "INSERT INTO \"user\" (id,first_name,last_name,birthday,location,creation_date) VALUES ($1,$2,$3,$4,$5,$6) RETURNING id,first_name,last_name,birthday,numeric_id ,location,creation_date";
+  try {
+    const res = await pool.query(query, [
+      id,
+      first_name,
+      last_name,
+      birthday,
+      location,
+      new Date(),
+    ]);
+    return res.rows[0];
+  } catch (error) {
+    winston.error("error", error);
+  }
+
+}

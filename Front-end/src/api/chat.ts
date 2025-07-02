21111a415b16629cdccd71bc5eedec656d9b2e6e
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-07-02 19:53:18 +0300

    fix : syntax on post to chat api

--- Front-end/src/api/chat.ts
+++ Front-end/src/api/chat.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 
-export const api_url = process.env.api_url || "https://sendit-production-87d4.up.railway.app";
+export const api_url = import.meta.env.api_url || "https://sendit-production-87d4.up.railway.app";
 
 export const getOrCreatePrivateChat = async (firstUserId: number|undefined, secondUserId: number|undefined) => {
   const res = await axios.get(api_url+"/api/chat", {
     params: { first_user_id: firstUserId, second_user_id: secondUserId },
   });
-
+  console.log(res.data);
+  
   if (!res.data && firstUserId && secondUserId) {
     const newChat = { firstUserId, secondUserId };
-    const resNew = await axios.post(api_url, newChat);
+    const resNew = await axios.post(api_url+"/api/chat", newChat);
     return resNew.data;
   }
 
